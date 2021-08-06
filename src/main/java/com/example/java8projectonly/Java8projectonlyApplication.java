68805ac509cd112d06c45293ab8a62bd38ca815b
cmudduluru <chandrika.mudduluru@actian.com>
2021-08-06 13:15:27 -0500

    Initial commit

---
+++ src/main/java/com/example/java8projectonly/Java8projectonlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.java8projectonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8projectonlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8projectonlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java8projectonly/Java8projectonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8projectonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8projectonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
